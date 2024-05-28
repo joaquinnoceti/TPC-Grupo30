60787c7f76464b6c4f13992a09565f766aa2a45b
glogatti <logattigustavo@gmail.com>
2024-05-28 15:03:23 -0300

    Creación clase Persona y clase Empleado

---
+++ TPCGrupo30/dominio/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Empleado : Persona
+    {
+        public int legajo { get; set; }
+        public DateTime fechaRegistro { get; set; }
+        public string nombreUsuario { get; set; }
+    }
+}
---
+++ TPCGrupo30/dominio/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+
+        public string nacionalidad { get; set; }
+    }
+}
