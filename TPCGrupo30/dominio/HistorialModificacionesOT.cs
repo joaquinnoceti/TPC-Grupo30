954f5cfd8de205d76bb003b6d18e815beee25bfa
glogatti <logattigustavo@gmail.com>
2024-07-07 21:11:26 -0300

    creacion clase observacionesOT e HistorialModificacionesOT

---
+++ TPCGrupo30/dominio/HistorialModificacionesOT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class HistorialModificacionesOT
+    {
+        public int ID { get; set; }
+        public int IDOrdenDeTrabajo { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public string CampoModificado { get; set; }
+        public string ValorAnterior { get; set; }
+        public string ValorNuevo { get; set; }
+        public int ModificadoPor { get; set; }
+    }
+}
---
+++ TPCGrupo30/dominio/ObservacionesOT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class ObservacionesOT
+    {
+        public int ID { get; set; }
+        public int IDOrdenDeTrabajo { get; set; }
+        public DateTime FechaObservacion { get; set; }
+        public string Observacion { get; set; }
+        public int IDEmpleado { get; set; }
+    }
+}
